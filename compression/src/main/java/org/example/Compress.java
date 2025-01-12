c873162a5bc7dec7ab75ad5ed20fa1b743e4f596
LeenJaber1 <jaberleen95@gmail.com>
2025-01-12 19:05:27 +0300

    compress feature

--- compression/src/main/java/org/example/Compress.java
+++ compression/src/main/java/org/example/Compress.java
@@ -1,5 +1,16 @@
 package org.example;
 
 public class Compress {
-
+    public String compressString(String word) {
+        String computedString = "";
+        for (int i = 0; i < word.length(); i++) {
+            int count = 1;
+            while (i + 1 < word.length() && word.charAt(i) == word.charAt(i + 1) && count < 9) {
+                count++;
+                i++;
+            }
+            computedString += (count + "" + word.charAt(i));
+        }
+        return computedString;
+    }
 }
