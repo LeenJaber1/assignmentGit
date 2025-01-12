991cb985bac3b674eee129e44cbd849e924795a8
LeenJaber1 <jaberleen95@gmail.com>
2025-01-12 19:08:23 +0300

    second developer commit

--- compression/src/main/java/org/example/Compress.java
+++ compression/src/main/java/org/example/Compress.java
@@ -13,4 +13,48 @@ public class Compress {
         }
         return computedString;
     }
+    public String decompressString(String compressedString) {
+        String ogString = "";
+        for(int i = 0 ; i < compressedString.length() ; i+=2){
+            String number = String.valueOf(compressedString.charAt(i));
+            int howMany = Integer.parseInt(number);
+            ogString += String.valueOf(compressedString.charAt(i+1)).repeat(howMany);
+        }
+        return ogString;
+    }
+
+    public int getNumber(char character){
+        String number = String.valueOf(character);
+        return Integer.parseInt(number);
+    }
+
+
+
+
+    private boolean isNumber(char character){
+        return character >= '1' && character<='9';
+    }
+
+
+
+
+    private boolean isLetter(char character){
+        return character >= 'a' && character <= 'z';
+    }
+
+
+    public boolean isValid(String compressedString) {
+        if(compressedString.length() % 2 != 0){
+            return false;
+        }
+        for(int i = 0 ; i < compressedString.length() ; i+=2){
+            if(!isNumber(compressedString.charAt(i))){
+                return false;
+            }
+            if(!isLetter(compressedString.charAt(i + 1))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
