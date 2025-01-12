05b13e6eb55ecf79de6c52243d80bf25a5d97f01
LeenJaber1 <jaberleen95@gmail.com>
2025-01-12 19:11:48 +0300

    hotfix commit

--- compression/src/main/java/org/example/Main.java
+++ compression/src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args){
 
+        Compress compress = new Compress();
+        String word = "hhhhhhhwwwwwnnn";
+        String compressedString = compress.compressString(word);
+        System.out.println("Compressed String = "  + compressedString);
+        System.out.println("Original String = " + word);
     }
 }
